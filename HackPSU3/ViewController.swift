fc2fbf933cdac8977b897410c1abfc51d0716976
finnmcsorley <finnmcsorley@psu.edu>
2020-11-07 21:46:06 -0500

    Only has authentication still working on table view cards

--- HackPSU3/ViewController.swift
+++ HackPSU3/ViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FUIAuthDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,5 +17,19 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        if let authUI = FUIAuth.defaultAuthUI(){
+            authUI.providers = [FUIOAuth.appleAuthProvider()]
+            authUI.delegate = self
+            
+            let authViewController = authUI.authViewController()
+            self.present(authViewController, animated: true)
+        }
+    }
+    func authUI( authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?){
+        if let user = authDataResult?.user{
+            print("User: \(user.uid)")
+        }
+    }
 }
 
